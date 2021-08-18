12fbb9e5411a97fbe52be90b9a4ac1a509abd05a
KiHyeon-Hong <ghdrlgus96@gmail.com>
2021-08-18 11:31:17 +0900

    Update Test code
    Update Test code

--- src/performanceTestCode1.js
+++ src/performanceTestCode1.js
@@ -62,25 +62,33 @@ async function performanceTest() {
             testLeakCount = 1;
         }
 
-        fs.appendFileSync(__dirname + '/../files/performanceTestLog.log', `=== 테스트 데이터: ${testPassword}, 실제 유출 여부: ${testLeakCount == 0 ? true : false} ===\n`, 'utf8');
-        console.log(`=== 테스트 데이터: ${testPassword}, 실제 유출 여부: ${testLeakCount == 0 ? true : false} ===`);
+        fs.appendFileSync(
+            __dirname + '/../files/performanceTestLog.log',
+            `=== 테스트 데이터: ${testPassword}, 실제 유출 여부: ${testLeakCount == 0 ? '유출된 비밀번호' : '유출되지 않은 비밀번호'} ===\n`,
+            'utf8'
+        );
+        console.log(`=== 테스트 데이터: ${testPassword}, 실제 유출 여부: ${testLeakCount == 0 ? '유출된 비밀번호' : '유출되지 않은 비밀번호'} ===`);
 
         for (let j = 0; j < 10; j++) {
             await passwordClient.passwordValidation(testPassword).then(function (result) {
                 console.log(result);
 
-                fs.appendFileSync(__dirname + '/../files/performanceTestLog.log', `${j + 1} 번째 유출 여부 예측: ${result.predictPoint < 0.6 ? true : false}\n`, 'utf8');
-                console.log(`${j + 1} 번째 유출 여부 예측: ${result.predictPoint < 0.6 ? true : false}`);
+                fs.appendFileSync(
+                    __dirname + '/../files/performanceTestLog.log',
+                    `${j + 1} 번째 유출 여부 예측: ${result.predictPoint < 0.6 ? '유출된 비밀번호' : '유출되지 않은 비밀번호'}\n`,
+                    'utf8'
+                );
+                console.log(`${j + 1} 번째 유출 여부 예측: ${result.predictPoint < 0.6 ? '유출된 비밀번호' : '유출되지 않은 비밀번호'}`);
 
                 if (j == 9) {
                     if ((testLeakCount == 0 ? true : false) == (result.predictPoint < 0.6 ? true : false)) {
-                        fs.appendFileSync(__dirname + '/../files/performanceTestLog.log', `=== 예측 성공 여부: ${true} ===\n\n`, 'utf8');
-                        console.log(`=== 예측 성공 여부: ${true} ===\n\n`);
+                        fs.appendFileSync(__dirname + '/../files/performanceTestLog.log', `=== 예측 성공 여부: ${'예측 성공'} ===\n\n`, 'utf8');
+                        console.log(`=== 예측 성공 여부: ${'예측 성공'} ===\n\n`);
 
                         success++;
                     } else {
-                        fs.appendFileSync(__dirname + '/../files/performanceTestLog.log', `=== 예측 성공 여부: ${false} ===\n\n`, 'utf8');
-                        console.log(`=== 예측 성공 여부: ${false} ===\n\n`);
+                        fs.appendFileSync(__dirname + '/../files/performanceTestLog.log', `=== 예측 성공 여부: ${'예측 실패'} ===\n\n`, 'utf8');
+                        console.log(`=== 예측 성공 여부: ${'예측 실패'} ===\n\n`);
 
                         fail++;
                     }
@@ -89,8 +97,8 @@ async function performanceTest() {
         }
     }
 
-    fs.appendFileSync(__dirname + '/../files/performanceTestLog.log', `예측 성공: ${success}, 예측 실패: ${fail}, 예측 정확도 ${(success / (success + fail)) * 100}%`, 'utf8');
-    console.log(`예측 성공: ${success}, 예측 실패: ${fail}, 예측 정확도 ${(success / (success + fail)) * 100}%`);
+    fs.appendFileSync(__dirname + '/../files/performanceTestLog.log', `예측 성공: ${success}회, 예측 실패: ${fail}회, 예측 정확도 ${(success / (success + fail)) * 100}%`, 'utf8');
+    console.log(`예측 성공: ${success}회, 예측 실패: ${fail}회, 예측 정확도 ${(success / (success + fail)) * 100}%`);
 }
 
 performanceTest();
