de26b53124397dba126e6295d9c7b79676c33ad7
KiHyeon-Hong <ghdrlgus96@gmail.com>
2021-08-31 14:02:05 +0900

    create ../Password

--- src/performanceTestCode.js
+++ src/performanceTestCode.js
@@ -6,7 +6,8 @@
  */
 
 // 사용 모듈 import
-const PasswordSecurityClient = require('@kihyeon-hong/password_security_client');
+// const PasswordSecurityClient = require('@kihyeon-hong/password_security_client');
+const PasswordSecurityClient = require('../PasswordSecurityClient');
 const fs = require('fs');
 
 var passwordClient = new PasswordSecurityClient.PasswordSecurity.PasswordSecurity();
