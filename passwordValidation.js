df71285a7daacf811466734e41c985ad054ceeec
KiHyeon-Hong <ghdrlgus96@gmail.com>
2021-10-04 10:46:05 +0900

    create passwordValidation.js

--- passwordValidation.js
+++ passwordValidation.js
@@ -2,7 +2,7 @@
     비밀번호 보안성 예측모델 예시 코드
 */
 
-const PasswordSecurityClient = require('@kihyeon-hong/password_security_client');
+const PasswordSecurityClient = require(__dirname + '/PasswordSecurity');
 var pwd = new PasswordSecurityClient.PasswordSecurity.PasswordSecurity();
 
 pwd.passwordValidation('q1w2e3r4').then(function (result) {
